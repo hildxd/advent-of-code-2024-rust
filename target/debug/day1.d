0c6712d24cf1a37334989da3c3ea4c3c77d7fdbc
hildxd <hildxd@outlook.com>
2024-12-02 13:33:40 +0800

    chore: add git ignore

--- target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/hildxd/Desktop/advent-of-code-2024-rust/target/debug/day1: /Users/hildxd/Desktop/advent-of-code-2024-rust/day1/src/main.rs
--- target/debug/deps/day1-07bdcb860a06f534.d
+++
@@ -1,5 +0,0 @@
-/Users/hildxd/Desktop/advent-of-code-2024-rust/target/debug/deps/day1-07bdcb860a06f534: day1/src/main.rs
-
-/Users/hildxd/Desktop/advent-of-code-2024-rust/target/debug/deps/day1-07bdcb860a06f534.d: day1/src/main.rs
-
-day1/src/main.rs:
